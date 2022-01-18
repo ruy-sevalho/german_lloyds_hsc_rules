59f06cd053b4981efdd5aa6c404d24fa0bc4ec67
ruy-sevalho <ruy.sevalho@gmail.com>
2022-01-18 14:21:06 -0300

    Update elements.py
    Include deserializer options for fields

--- src/gl_hsc_scantling/elements.py
+++ src/gl_hsc_scantling/elements.py
@@ -6,18 +6,29 @@ Created on Fri Aug  7 15:36:31 2020
 
 CODE FOR USING German Lloyd 2012 High Speed Craft strucutural rules
 # """
+from dataclasses import dataclass, field
 from enum import Enum
 
 import numpy as np
+from dataclass_tools.tools import DESERIALIZER_OPTIONS, DeSerializerOptions
 
-from .dc import dataclass
-from .vessel import Vessel
-from .structural_model import StructuralModel
 from .locations_abc import Location
+from .named_field import NAMED_FIELD_OPTIONS
+from .panels import Panel
+from .stiffeners import AttStiffenerSection
+from .structural_model import StructuralModel
+from .vessel import Vessel
 
 
 def _distance(start, end):
     return end - start
+
+
+MODEL_TYPE_TABLE = {
+    Panel.__name__: Panel,
+    AttStiffenerSection.__name__: AttStiffenerSection,
+}
+MODEL_OPTIONS = DeSerializerOptions(add_type=True, subtype_table=MODEL_TYPE_TABLE)
 
 
 @dataclass
@@ -27,8 +38,8 @@ class StructuralElement:
     name: str
     x: float
     z: float
-    vessel: Vessel
-    model: StructuralModel
+    vessel: Vessel = field(metadata={DESERIALIZER_OPTIONS: NAMED_FIELD_OPTIONS})
+    model: StructuralModel = field(metadata={DESERIALIZER_OPTIONS: MODEL_OPTIONS})
     location: Location
 
     @property
