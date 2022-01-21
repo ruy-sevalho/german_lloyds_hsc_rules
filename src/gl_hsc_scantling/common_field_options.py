99447c6da3a02334c0b8bbd37274ade89592e06a
ruy-sevalho <ruy.sevalho@gmail.com>
2022-01-21 08:50:58 -0300

    Rename named_field common_field_options

--- src/gl_hsc_scantling/common_field_options.py
+++ src/gl_hsc_scantling/common_field_options.py
@@ -1,3 +1,6 @@
 from dataclass_tools.tools import DeSerializerOptions
 
 NAMED_FIELD_OPTIONS = DeSerializerOptions(subs_by_attr="name")
+LAMINATE_OPTIONS = DeSerializerOptions(
+    subs_by_attr="name", subs_collection_name="laminates"
+)
