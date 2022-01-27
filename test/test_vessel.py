692f4a9c178bc088a09e53127216d009792c998a
ruy-sevalho <ruy.sevalho@gmail.com>
2022-01-27 13:26:28 -0300

    Update test_vessel.py
    Add type hint to test.

--- test/test_vessel.py
+++ test/test_vessel.py
@@ -5,9 +5,12 @@ Created on Wed May 26 18:16:39 2021
 @author: ruy
 """
 import pytest as pt
+from gl_hsc_scantling.vessel import Vessel
+
+from .exp_output import ExpVessel
 
 
-def test_vessel_ex1(vessel_ex1, vessel_ex1_expected):
+def test_vessel_ex1(vessel_ex1: Vessel, vessel_ex1_expected: ExpVessel):
     """Global loads check."""
     assert vessel_ex1.vert_acg == pt.approx(vessel_ex1_expected.vert_acg)
     assert vessel_ex1.max_wave_height == pt.approx(vessel_ex1_expected.max_wave_height)
